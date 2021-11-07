b99710c65289114a854a454e8699b197543cd8fe
akiotsuji <42998119+akiotsuji@users.noreply.github.com>
2021-11-07 15:15:03 +0900

    Update and rename awesome-install.sh to install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #! /usr/bin/env zsh
 
 sudo apt update
-sudo apt upgrade
-sudo apt install awesome scrot xclip brightnessctl acpi git
+sudo apt upgrade -y
+sudo apt install awesome scrot xclip brightnessctl acpi git xfonts-termius
 git clone https://github.com/akiotsuji/awesome-wm.git ~/.config/
+mkdir ~/.config
+cd ~/.config
 mv ~/.config/awesome-wm ~/.config/awesome
 cd ~/.config/awesome
-cat Xinitrc >> ~/.Xinitrc
+cp Xinitrc ~/.Xinitrc
 cp Xdefault ~/.Xdefault
 mkdir -p ~/Pictures
