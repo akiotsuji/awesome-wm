9302fe647905d6bfb0f48e954ad168c01a6caba5
akiotsuji <akio9712@gmail.com>
2021-11-12 14:49:52 +0900

    change rule

--- rc.lua
+++ rc.lua
@@ -422,6 +422,7 @@ globalkeys = gears.table.join(
     -- Standard program
     awful.key({ "Control",           }, "Return", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
+    awful.key({ "Control",  "Mod1"   }, "Return", function () awful.util.spawn("google-chrome") end),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
